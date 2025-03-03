23ef1bc74a48188ebc49ccf06a1bc398324b8997
trananhduck <anhduc9b1cva@gmail.com>
2025-03-03 21:10:43 +0700

    them CSRF + config.php

---
+++ admin/inc/config.php
@@ -0,0 +1,31 @@
+<?php
+// Bật hiển thị lỗi
+ini_set('error_reporting', E_ALL);
+
+// Thiết lập múi giờ
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+
+// Tên máy chủ
+$dbhost = 'localhost';
+
+// Tên cơ sở dữ liệu
+$dbname = 'gobuy';
+
+// Tên người dùng cơ sở dữ liệu
+$dbuser = 'root';
+
+// Mật khẩu cơ sở dữ liệu
+$dbpass = '';
+
+// Định nghĩa đường dẫn gốc
+define("BASE_URL", "");
+
+// Lấy đường dẫn đến trang admin
+define("ADMIN_URL", BASE_URL . "admin" . "/");
+
+try {
+    $pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $exception) {
+    echo "Lỗi kết nối: " . $exception->getMessage();
+}
