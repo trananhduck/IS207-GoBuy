4a03caf157cc2f96234e893d2b4faaf9a5aec7c9
trananhduck <anhduc9b1cva@gmail.com>
2025-03-06 11:57:51 +0700

    giao diện + chức năng update profile

---
+++ customer-sidebar.php
@@ -0,0 +1,11 @@
+<div class="user-sidebar">
+    <ul>
+        <a href="customer-profile-update.php">
+            <a href="customer-billing-shipping-update.php"><button
+                    class="btn btn-danger"><?php echo 'Cập nhật thông tin giao hàng' ?></button></a>
+            <a href="customer-password-update.php"><button
+                    class="btn btn-danger"><?php echo 'Đổi mật khẩu' ?></button></a>
+            <a href="customer-order.php"><button class="btn btn-danger"><?php echo 'Hàng đã đặt' ?></button></a>
+            <a href="logout.php"><button class="btn btn-danger"><?php echo 'Đăng xuất' ?></button></a>
+    </ul>
+</div>
