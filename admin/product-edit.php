4a60f952352f9550044ba57a38583e9483ee9e4e
trananhduck <anhduc9b1cva@gmail.com>
2025-03-12 20:58:05 +0700

    abc

--- admin/product-edit.php
+++ admin/product-edit.php
@@ -260,3 +260,311 @@ foreach ($result as $row) {
     $color_id[] = $row['color_id'];
 }
 ?>
+
+
+<section class="content">
+
+    <div class="row">
+        <div class="col-md-12">
+
+            <?php if ($errorMsg): ?>
+            <div class="callout callout-danger">
+
+                <p>
+                    <?php echo $errorMsg; ?>
+                </p>
+            </div>
+            <?php endif; ?>
+
+            <?php if ($successMsg): ?>
+            <div class="callout callout-success">
+
+                <p><?php echo $successMsg; ?></p>
+            </div>
+            <?php endif; ?>
+
+            <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
+
+                <div class="box box-info">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Tên danh mục cấp cao nhất
+                                <span>*</span></label>
+                            <div class="col-sm-4">
+                                <select name="tcat_id" class="form-control select2 top-cat">
+                                    <option value="">Chọn danh mục cấp cao nhất</option>
+                                    <?php
+                                    $statement = $pdo->prepare("SELECT * FROM table_top_category ORDER BY tcat_name ASC");
+                                    $statement->execute();
+                                    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+                                    foreach ($result as $row) {
+                                    ?>
+                                    <option value="<?php echo $row['tcat_id']; ?>" <?php if ($row['tcat_id'] == $tcat_id) {
+                                                                                            echo 'selected';
+                                                                                        } ?>>
+                                        <?php echo $row['tcat_name']; ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Tên danh mục cấp trung <span>*</span></label>
+                            <div class="col-sm-4">
+                                <select name="mcat_id" class="form-control select2 mid-cat">
+                                    <option value="">Chọn danh mục cấp trung</option>
+                                    <?php
+                                    $statement = $pdo->prepare("SELECT * FROM table_mid_category WHERE tcat_id = ? ORDER BY mcat_name ASC");
+                                    $statement->execute(array($tcat_id));
+                                    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+                                    foreach ($result as $row) {
+                                    ?>
+                                    <option value="<?php echo $row['mcat_id']; ?>" <?php if ($row['mcat_id'] == $mcat_id) {
+                                                                                            echo 'selected';
+                                                                                        } ?>>
+                                        <?php echo $row['mcat_name']; ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Tên danh mục cấp cuối <span>*</span></label>
+                            <div class="col-sm-4">
+                                <select name="ecat_id" class="form-control select2 end-cat">
+                                    <option value="">Chọn danh mục cấp cuối</option>
+                                    <?php
+                                    $statement = $pdo->prepare("SELECT * FROM table_end_category WHERE mcat_id = ? ORDER BY ecat_name ASC");
+                                    $statement->execute(array($mcat_id));
+                                    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+                                    foreach ($result as $row) {
+                                    ?>
+                                    <option value="<?php echo $row['ecat_id']; ?>" <?php if ($row['ecat_id'] == $ecat_id) {
+                                                                                            echo 'selected';
+                                                                                        } ?>>
+                                        <?php echo $row['ecat_name']; ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Tên sản phẩm <span>*</span></label>
+                            <div class="col-sm-4">
+                                <input type="text" name="p_name" class="form-control" value="<?php echo $p_name; ?>">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Giá cũ<br><span
+                                    style="font-size:10px;font-weight:normal;">(Tính bằng USD)</span></label>
+                            <div class="col-sm-4">
+                                <input type="text" name="p_old_price" class="form-control"
+                                    value="<?php echo $p_old_price; ?>">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Giá hiện tại <span>*</span><br><span
+                                    style="font-size:10px;font-weight:normal;">(Tính bằng USD)</span></label>
+                            <div class="col-sm-4">
+                                <input type="text" name="p_current_price" class="form-control"
+                                    value="<?php echo $p_current_price; ?>">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Số lượng <span>*</span></label>
+                            <div class="col-sm-4">
+                                <input type="text" name="p_qty" class="form-control" value="<?php echo $p_qty; ?>">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Chọn kích thước</label>
+                            <div class="col-sm-4">
+                                <select name="size[]" class="form-control select2" multiple="multiple">
+                                    <?php
+									$is_select = '';
+									$statement = $pdo->prepare("SELECT * FROM table_size ORDER BY size_id ASC");
+									$statement->execute();
+									$result = $statement->fetchAll(PDO::FETCH_ASSOC);			
+									foreach ($result as $row) {
+										if(isset($size_id)) {
+											if(in_array($row['size_id'],$size_id)) {
+												$is_select = 'selected';
+											} else {
+												$is_select = '';
+											}
+										}
+										?>
+                                    <option value="<?php echo $row['size_id']; ?>" <?php echo $is_select; ?>>
+                                        <?php echo $row['size_name']; ?></option>
+                                    <?php
+									}
+									?>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Chọn màu sắc</label>
+                            <div class="col-sm-4">
+                                <select name="color[]" class="form-control select2" multiple="multiple">
+                                    <?php
+                                    $is_select = '';
+                                    $querry = $pdo->prepare("SELECT * FROM table_color ORDER BY color_id ASC");
+                                    $querry->execute();
+                                    $result = $querry->fetchAll(PDO::FETCH_ASSOC);
+                                    foreach ($result as $row) {
+                                        if (isset($color_id)) {
+                                            if (in_array($row['color_id'], $color_id)) {
+                                                $is_select = 'selected';
+                                            } else {
+                                                $is_select = '';
+                                            }
+                                        }
+                                    ?>
+                                    <option value="<?php echo $row['color_id']; ?>" <?php echo $is_select; ?>>
+                                        <?php echo $row['color_name']; ?>
+                                    </option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Ảnh nổi bật hiện tại</label>
+                            <div class="col-sm-4" style="padding-top:4px;">
+                                <img src="../assets/uploads/<?php echo $p_featured_photo; ?>" alt=""
+                                    style="width:150px;">
+                                <input type="hidden" name="current_photo" value="<?php echo $p_featured_photo; ?>">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Thay đổi ảnh nổi bật</label>
+                            <div class="col-sm-4" style="padding-top:4px;">
+                                <input type="file" name="p_featured_photo">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Ảnh khác</label>
+                            <div class="col-sm-4" style="padding-top:4px;">
+                                <table id="ProductTable" style="width:100%;">
+                                    <tbody>
+                                        <?php
+                                        $querry = $pdo->prepare("SELECT * FROM table_product_photo WHERE p_id=?");
+                                        $querry->execute(array($_REQUEST['id']));
+                                        $result = $querry->fetchAll(PDO::FETCH_ASSOC);
+                                        foreach ($result as $row) {
+                                        ?>
+                                        <tr>
+                                            <td>
+                                                <img src="../assets/uploads/product_photos/<?php echo $row['photo']; ?>"
+                                                    alt="" style="width:150px;margin-bottom:5px;">
+                                            </td>
+                                            <td style="width:28px;">
+                                                <a onclick="return confirmDelete();"
+                                                    href="product-other-photo-delete.php?id=<?php echo $row['pp_id']; ?>&id1=<?php echo $_REQUEST['id']; ?>"
+                                                    class="btn btn-danger btn-xs">Xóa</a>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="col-sm-2">
+                                <input type="button" id="btnAddNew" value="Thêm mục"
+                                    style="margin-top: 5px;margin-bottom:10px;border:0;color: #fff;font-size: 14px;border-radius:3px;"
+                                    class="btn btn-warning btn-xs">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Mô tả</label>
+                            <div class="col-sm-8">
+                                <textarea name="p_description" class="form-control" cols="30" rows="10"
+                                    id="editor1"><?php echo $p_description; ?></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Mô tả ngắn</label>
+                            <div class="col-sm-8">
+                                <textarea name="p_short_description" class="form-control" cols="30" rows="10"
+                                    id="editor1"><?php echo $p_short_description; ?></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Tính năng</label>
+                            <div class="col-sm-8">
+                                <textarea name="p_feature" class="form-control" cols="30" rows="10"
+                                    id="editor3"><?php echo $p_feature; ?></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Chính sách hoàn trả</label>
+                            <div class="col-sm-8">
+                                <textarea name="p_return_policy" class="form-control" cols="30" rows="10"
+                                    id="editor5"><?php echo $p_return_policy; ?></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Sản phẩm nổi bật?</label>
+                            <div class="col-sm-8">
+                                <select name="p_is_featured" class="form-control" style="width:auto;">
+                                    <option value="0" <?php if ($p_is_featured == '0') {
+                                                            echo 'selected';
+                                                        } ?>>Không</option>
+                                    <option value="1" <?php if ($p_is_featured == '1') {
+                                                            echo 'selected';
+                                                        } ?>>Có</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label">Sản phẩm đang hoạt động?</label>
+                            <div class="col-sm-8">
+                                <select name="p_is_active" class="form-control" style="width:auto;">
+                                    <option value="0" <?php if ($p_is_active == '0') {
+                                                            echo 'selected';
+                                                        } ?>>Không</option>
+                                    <option value="1" <?php if ($p_is_active == '1') {
+                                                            echo 'selected';
+                                                        } ?>>Có</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="col-sm-3 control-label"></label>
+                            <div class="col-sm-6">
+                                <button type="submit" class="btn btn-success pull-left" name="form1">Cập nhật</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </form>
+
+
+        </div>
+    </div>
+
+</section>
+
+<?php require_once('footer.php'); ?>
